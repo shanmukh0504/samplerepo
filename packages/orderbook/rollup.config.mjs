4cb9c733f5f578c169ba3634c802b66123f7b5ae
shanmukh0504 <shanmukh0504@gmail.com>
2025-04-01 14:09:36 +0530

    patch: new patch

--- packages/orderbook/rollup.config.mjs
+++ packages/orderbook/rollup.config.mjs
@@ -1,32 +1,22 @@
-import alias from '@rollup/plugin-alias';
-import path from 'path';
-import typescript from 'rollup-plugin-typescript2';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: [
     {
-      file: 'dist/index.js',
-      format: 'cjs',
+      file: "dist/index.js",
+      format: "cjs",
       sourcemap: true,
     },
     {
-      file: 'dist/index.esm.js',
-      format: 'esm',
+      file: "dist/index.esm.js",
+      format: "esm",
       sourcemap: true,
     },
   ],
   plugins: [
-    alias({
-      entries: [
-        {
-          find: 'utils',
-          replacement: path.resolve(__dirname, '../utils/src')
-        }
-      ]
-    }),
     resolve(),
     commonjs(),
     typescript({
